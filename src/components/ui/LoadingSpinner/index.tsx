a4ddfab9eecf3f1ee7c7b7bd71eda0facc996e09
Mykola Kolomoyets <mykola.kolomoyets@phenomenon-studio.com>
2026-05-13 12:59:46 +0300

    fix(spinner): replaced spinner with masked div

--- src/components/ui/LoadingSpinner/index.tsx
+++ src/components/ui/LoadingSpinner/index.tsx
@@ -1,8 +1,7 @@
-import SpinnerGradient32Icon from '@/icons/spinner-gradient_32.svg?react';
 import clsx from 'clsx';
 import { WithClassName } from '@/lib/types';
 import s from './styles.module.css';
 
 export const LoadingSpinner: React.FC<WithClassName> = ({ className }) => {
-    return <SpinnerGradient32Icon className={clsx(s.wrap, className)} aria-hidden />;
+    return <div className={clsx(s.wrap, className)} aria-hidden />;
 };
